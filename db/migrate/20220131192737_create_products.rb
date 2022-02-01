29be0f92104e1e7d20fb75222c1da402b76e0711
Daniel Herrera <daniel97hh@gmail.com>
2022-02-01 10:46:48 -0500

    Se corrigen validaciones en el modelo Product y se crean seeds

--- db/migrate/20220131192737_create_products.rb
+++ db/migrate/20220131192737_create_products.rb
@@ -7,7 +7,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
       t.float :price
       t.integer :stock
       t.string :bar_code
-      t.integer :minimun_required
+      t.integer :minimum_required
       t.float :cost
       t.boolean :visible, default: true
       t.references :warehouse, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+categories = %w[Polos Jeans Shorts Camisas Chompas Vestidos Faldas Blusas Casacas Abrigos Trajes Bufandas Pijamas Bodies Fajas]
+colors = %w[amarillo verde azul rojo negro blanco gris rosado morado carmesi magenta champagne indigo coral turquesa salmon beige lila]
+sizes = %w[XS S M L XL]
+prep = %w[de con sin para tipo]
+final = ["costuras", "malla", "cuello scoop bajo", "talle alto", "escote", "estampado", "bolsillo", "cuello unicolor", "cremallera delantera",
+        "dibujos animados", "parche", "aislamiento termico", "plumon engrosado", "manga larga", "manga corta", "atletico", "gemas del infinito",
+        "poderes", "wind power", "girl power", "bichota", "bunny", "classic", "aesthetic", "moonpay", "boleta", "menol", "free britney", "RiRi"]
+
+50.times do
+  random_category = categories.sample
+  name = "#{random_category[..-2]} #{colors.sample} #{prep.sample} #{final.sample}"
+  bar_code =  Faker::Barcode.ean
+  cost = rand(20..40.0).round(2)
+  price = rand(50..60.0).round(2)
+  category = random_category
+  size = sizes.sample
+  stock = rand(10..20)
+  minimum_required = rand(10..15)
+
+  product_attr = {
+    name: name,
+    bar_code: bar_code,
+    cost: cost,
+    price: price,
+    category: category,
+    size: size,
+    stock: stock,
+    minimum_required: minimum_required
+  }
+
+  new_product = Product.new(product_attr)
+  new_product.warehouse = User.first.warehouse
+end
