f7e5a497939335a470cb6633621d399518c34a30
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-03 12:11:12 -0500

    se puso metodos protegidos en application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user! # para autenticar - quitar luego el comment
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
+  protected
 
   def after_sign_in_path_for(_resource)
     dashboard_path
