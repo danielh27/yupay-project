fffde521017d5cc5fc3a3852d5db2bd7320cce58
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-03 16:24:23 -0500

    se hizo relacion de bajo stocj y alto stock

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,13 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @products = Product.all
+    if params[:query].present?
+      if params[:query] == '2'
+      @products = Product.all.sort_by{ |product| product.stock }.take(5)
+      elsif params[:query] == '3'
+      @products = Product.all.sort_by{ |product| -product.stock}.take(5)
+      end
+    end
   end
 end
