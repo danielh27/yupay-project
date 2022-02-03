7fdc7a08e9e3b4111b15209578b103e7ee0f3fa4
Daniel Herrera <daniel97hh@gmail.com>
2022-02-03 14:07:27 -0500

    se corrige error en el formulario al momento de validar

--- app/controllers/list_orders_controller.rb
+++ app/controllers/list_orders_controller.rb
@@ -6,12 +6,12 @@ class ListOrdersController < ApplicationController
   end
 
   def create
-    order = Order.find(params[:order_id])
+    @order = Order.find(params[:order_id])
     @list_order = ListOrder.new(list_order_params)
     @list_orders = ListOrder.where(order: params[:order_id])
-    @list_order.order = order
+    @list_order.order = @order
     if @list_order.save
-      redirect_to order_list_orders_path(order)
+      redirect_to order_list_orders_path(@order)
     else
       render :index
     end
