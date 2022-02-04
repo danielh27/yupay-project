7091de149058587def6d65cca65c510f3247ca82
Daniel Herrera <daniel97hh@gmail.com>
2022-02-04 12:57:20 -0500

    estructura de dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,17 +5,16 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @products = Product.all
     if params[:query].present?
       if params[:query] == '2'
-        @products = Product.all.order(stock: :asc).take(5)
+        @products = Product.all.order(stock: :asc).limit(5)
       elsif params[:query] == '3'
-        @products = Product.all.order(stock: :desc).take(5)
+        @products = Product.all.order(stock: :desc).limit(5)
       else
         @products = Product.all.sort_by { |product| -product.list_orders.sum(:quantity) }.take(5)
       end
     else
-      @products = Product.all
+      @products = Product.all.limit(5)
     end
   end
 
@@ -23,7 +22,7 @@ class PagesController < ApplicationController
     # variables pasadas
     # @list_purchases = ListPurchase.includes(:purchase)
     # @confirmed_lists = @list_purchases.select {|list| list.purchase.status = true }
-    
+
     # purchases
     @confirmed_purchases = Purchase.where(status: true)
     # ordenes
