8ff55ef19049dbe03c7dba26407d6ec2b71d4d92
Daniel Herrera <daniel97hh@gmail.com>
2022-02-06 02:16:37 -0500

    se agrega barra de busqueda para productos

--- app/controllers/list_orders_controller.rb
+++ app/controllers/list_orders_controller.rb
@@ -3,6 +3,16 @@ class ListOrdersController < ApplicationController
     @list_order = ListOrder.new
     @list_orders = ListOrder.where(order: params[:order_id])
     @order = Order.find(params[:order_id])
+    @products = Product.order(price: :desc)
+
+    if params[:query].present?
+      @products = Product.where('name ILIKE :query OR bar_code ILIKE :query', query: "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "results.html" }
+    end
   end
 
   def create
---
+++ app/javascript/controllers/refresh_products_list_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["form", "results", "searchInput"]
+
+  // connect() {
+  //   console.log(this.element);
+  //   console.log(this.formTarget);
+  //   console.log(this.resultsTarget);
+  //   console.log(this.searchInputTarget);
+  // }
+
+  update() {
+    const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.resultsTarget.outerHTML = data;
+      })
+  }
+}
