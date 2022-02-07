8cb67c470a475cfc4ed3ad8e31f318b6590c0a9e
Daniel Herrera <daniel97hh@gmail.com>
2022-02-06 19:52:00 -0500

    se avanza columna de seleccion de productos en la seccion de ventas

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = ["items", "form", "dropdownProduct", "dropdownQuantity"];
+  static targets = ["items", "dropdownProduct", "dropdownQuantity"];
 
   // connect() {
   //   console.log(this.element);
@@ -14,10 +14,10 @@ export default class extends Controller {
   send(event) {
     event.preventDefault();
 
-    fetch(this.formTarget.action, {
+    fetch(event.currentTarget.action, {
       method: 'POST',
       headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken() },
-      body: new FormData(this.formTarget)
+      body: new FormData(event.currentTarget)
     })
       .then(response => response.json())
       .then((data) => {
@@ -26,25 +26,25 @@ export default class extends Controller {
           li_element.innerHTML = data.inserted_item;
           this.itemsTarget.appendChild(li_element);
         }
-        this.formTarget.outerHTML = data.form;
+        event.currentTarget.outerHTML = data.form;
       });
   }
 
-  update_select_options(event) {
+  // update_select_options(event) {
 
-    while (this.dropdownQuantityTarget.length > 0) {
-      this.dropdownQuantityTarget.remove(0);
-    }
+  //   while (this.dropdownQuantityTarget.length > 0) {
+  //     this.dropdownQuantityTarget.remove(0);
+  //   }
 
-    fetch(`/products/${this.dropdownProductTarget.value}`, { method: 'GET', headers: { 'Accept': "application/json" }})
-      .then(response => response.json())
-      .then(data => {
-        for (let number = 1; number <= data.product_stock; number++) {
-          let option = document.createElement("option");
-          option.textContent = number;
-          option.value = number;
-          this.dropdownQuantityTarget.add(option);
-        }
-      });
-  }
+  //   fetch(`/products/${this.dropdownProductTarget.value}`, { method: 'GET', headers: { 'Accept': "application/json" }})
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       for (let number = 1; number <= data.product_stock; number++) {
+  //         let option = document.createElement("option");
+  //         option.textContent = number;
+  //         option.value = number;
+  //         this.dropdownQuantityTarget.add(option);
+  //       }
+  //     });
+  // }
 }
--- app/javascript/controllers/refresh_products_list_controller.js
+++ app/javascript/controllers/refresh_products_list_controller.js
@@ -10,7 +10,10 @@ export default class extends Controller {
   //   console.log(this.searchInputTarget);
   // }
 
+
+
   update() {
+
     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
