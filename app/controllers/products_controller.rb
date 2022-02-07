6626ca9e5675cb3d4d77a5706a728109df7bff53
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-07 12:53:49 -0500

    se termimno logica de la tabla

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,10 @@ class ProductsController < ApplicationController
     @list_purchase = ListPurchase.where(product_id: params[:id])
     @list_order = ListOrder.where(product_id: params[:id])
 
+    @total = @list_purchase + @list_order
+    @total.sort_by! { |p| p.created_at }
+    @total.reverse!
+
     respond_to do |format|
       format.html
       format.json { @product }
