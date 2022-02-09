5c2780eebcffb84023499bafef9bfad464f58939
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-09 15:43:30 -0500

    se corrige banner y navbar

--- app/controllers/list_orders_controller.rb
+++ app/controllers/list_orders_controller.rb
@@ -4,7 +4,7 @@ class ListOrdersController < ApplicationController
     # @list_orders = ListOrder.includes(:product).where(order: params[:order_id])
     @list_orders = policy_scope(ListOrder).includes(:product).where(order: params[:order_id])
     @order = Order.find(params[:order_id])
-    @products = Product.where(warehouse: current_user).order(price: :desc)
+    @products = policy_scope(Product).order(price: :desc)
     @total_sum = @list_orders.select(:price, :quantity).sum("price * quantity").round(2)
 
     if params[:query].present?
