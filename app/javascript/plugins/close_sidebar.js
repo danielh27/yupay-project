703b448a7fa6a04ae7364eb2458d9aa2b2270913
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-27 20:24:52 -0500

    se ordeno archivos de js

--- app/javascript/plugins/close_sidebar.js
+++ app/javascript/plugins/close_sidebar.js
@@ -1,7 +1,9 @@
 const closeSidebar = () => {
-  document.getElementById("accordionSidebarone").style.display = "none";
-  document.getElementById("menu-line").classList.toggle("line-none");
-  document.getElementById("menu-line").classList.add("w3-teal");
+  document.getElementById("accordionSidebarone").addEventListener("click", () => {
+    document.getElementById("accordionSidebarone").style.display = "none";
+    document.getElementById("menu-line").classList.toggle("line-none");
+    document.getElementById("menu-line").classList.add("w3-teal");
+  })
 }
 
 export {closeSidebar};
